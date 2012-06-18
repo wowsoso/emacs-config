7a9dd14cb58d396aaa47c586c27ec176568f60ab
Tutu Wang <wangqi@admaster.com.cn>
2012-06-18 23:31:49 +0800

    ibus init

--- init.el
+++ init.el
@@ -108,6 +108,22 @@
 (require 'weibo)
 
 
+;;;ibus
+(require 'ibus)
+;; Turn on ibus-mode automatically after loading .emacs
+(add-hook 'after-init-hook 'ibus-mode-on)
+;; Use C-SPC for Set Mark command
+(ibus-define-common-key ?\C-\s nil)
+;; Use C-/ for Undo command
+(ibus-define-common-key ?\C-/ nil)
+;; Change cursor color depending on IBus status
+(setq ibus-cursor-color '("red" "blue" "limegreen"))
+
+;; Use S-SPC to toggle input status
+(ibus-define-common-key ?\S-\s nil)
+(global-set-key (kbd "S-SPC") 'ibus-toggle)
+
+
 ;;; custom lib
          
 (mapcar (lambda (file)  (load-file (concat "~/.emacs.d/site/custom/" file)))
