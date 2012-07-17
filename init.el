271fb7c02468902337b68875698cf39e0a98db35
Tutu Wang <wangqi@admaster.com.cn>
2012-07-17 18:36:54 +0800

    sync from office

--- init.el
+++ init.el
@@ -91,10 +91,48 @@
                 scheme-mode-hook))
   (add-hook hook 'turn-on-auto-fill))
 
+;;;python mode
+(require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+
 ;;;pymacs
 (require 'pymacs)
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport t)
+
+;;;ipython
+(setq ipython-command "/usr/bin/ipython")
+(require 'ipython)
+
+;;;lambda mode
+;; (require 'lambda-mode)
+;; (add-hook 'python-mode-hook #'lambda-mode 1)
+;; (setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
+
+; anything
+(require 'anything)
+(require 'anything-ipython)
+(when (require 'anything-show-completion nil t)
+   (use-anything-show-completion 'anything-ipython-complete
+                                 '(length initial-pattern)))
+
+(require 'python-pep8)
+(require 'python-pylint)
+
+;;;comint
+(require 'comint)
+(define-key comint-mode-map (kbd "M-") 'comint-next-input)
+(define-key comint-mode-map (kbd "M-") 'comint-previous-input)
+(define-key comint-mode-map [down] 'comint-next-matching-input-from-input)
+(define-key comint-mode-map [up] 'comint-previous-matching-input-from-input)
+
+;;;autopair
+(autoload 'autopair-global-mode "autopair" nil t)
+(autopair-global-mode)
+(dolist (hook '(scheme-mode-hook
+                emacs-lisp-mode-hook
+                lisp-mode-hook))
+  (add-hook hook #'(lambda () (setq autopair-dont-activate t))))
 
 ;;;yasnippet
 (require 'yasnippet-bundle)
@@ -130,6 +168,6 @@
 
 
 ;;; custom lib
-         
+
 (mapcar (lambda (file)  (load-file (concat "~/.emacs.d/site/custom/" file)))
-        (list "screen.el" "dict.el" "charpair.el" "c-mode.el" "perl.el"))
+        (list "screen.el" "dict.el" "c-mode.el" "perl.el"))
